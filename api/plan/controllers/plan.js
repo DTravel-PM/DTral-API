67299d09b1622d5958d5af8022e74504781c8a7d
Nguyen Tan Dat <datnguyen25082000@gmail.com>
2021-12-06 21:53:59 +0700

    NTDat - create table

--- api/plan/controllers/plan.js
+++ api/plan/controllers/plan.js
@@ -1,8 +1,14 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  getPlanByUserId: async (ctx) => {
+    const { userid } = ctx.params;
+    const result = await strapi.services.plan.getPlanByUserId(userid);
+    return result;
+  },
+};
--- api/plan/services/plan.js
+++ api/plan/services/plan.js
@@ -1,8 +1,26 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  getPlanByUserId: async (userid) => {
+    const result = await strapi.query("plan").find({ userid: userid });
+
+    if (result) {
+      return result.map((item) => {
+        return {
+          id: item.id,
+          title: item.title,
+          description: item.description,
+          userid: item.userid,
+          planid: item.planid,
+        };
+      });
+    }
+
+    return [];
+  },
+};
