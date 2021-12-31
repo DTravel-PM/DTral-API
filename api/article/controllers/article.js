68a8562a25a7aaf84671ee6809da9b17546bf852
Nguyen Tan Dat <datnguyen25082000@gmail.com>
2021-12-31 11:09:59 +0700

    NTdat = create-post

--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -1,8 +1,14 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  search: async (ctx) => {
+    const { text } = ctx.query;
+    const result = await strapi.services.article.searchPost(text);
+    return result;
+  },
+};
--- api/article/services/article.js
+++ api/article/services/article.js
@@ -1,8 +1,31 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  searchPost: async (str) => {
+    const result = await strapi.query("article").find({});
+    const text = str.toLowerCase();
+    console.log("text", text);
+    if (result) {
+      let res = [];
+      result.forEach((element) => {
+        if (
+          element.title?.toLowerCase().includes(text) ||
+          element.content?.toLowerCase().includes(text) ||
+          element.location?.toLowerCase().includes(text) ||
+          element.summary?.toLowerCase().includes(text)
+        ) {
+          res.push(element);
+        }
+      });
+
+      return res;
+    }
+
+    return [];
+  },
+};
